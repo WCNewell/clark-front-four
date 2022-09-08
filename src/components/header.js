8ad8211ba5eb11206c0f37874fdf48d1c9b033d0
William Clark Newell <clark@clarknewell.tech>
2022-09-08 13:24:46 -0600

    Working on new frontend, starting with header
    Working on new frontend, starting with header

--- src/components/header.js
+++ src/components/header.js
@@ -1,28 +1,39 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
+import Logo from '../assets/clarklogo.inline.svg'
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      margin: `0 auto`,
-      padding: `var(--space-4) var(--size-gutter)`,
-      display: `flex`,
-      alignItems: `center`,
-      justifyContent: `space-between`,
-    }}
-  >
-    <Link
+const Header = ({siteTitle}) => {
+  const [isSticky, setSticky] = React.useState(false)
+  const ref = React.useRef(null)
+  const handleScroll = () => {
+    if (ref.current) {
+        setSticky(ref.current.getBoundingClientRect().top <= 0)
+    }
+  }
+
+  React.useEffect(() => {
+    window.addEventListener('scroll', handleScroll)
+
+    return () => {
+        window.removeEventListener('scroll', () => handleScroll)
+    }
+  }, []);
+  
+  <header>
+   <Link
       to="/"
       style={{
         fontSize: `var(--font-sm)`,
         textDecoration: `none`,
       }}
     >
-      {siteTitle}
+      
+      <Logo />
+      <h2>{siteTitle}</h2>
     </Link>
   </header>
-)
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
