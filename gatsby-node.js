0a1fddec2176a1e276c0ad5b0751647f4a9571d7
William Clark Newell <clark@clarknewell.tech>
2022-09-10 18:47:44 -0600

    Installed typography
    Installed typography

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,32 @@
-exports.createPages = async ({ actions }) => {
+const path = require(`path`);
+
+exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
-  createPage({
-    path: "/using-dsg",
-    component: require.resolve("./src/templates/using-dsg.js"),
-    context: {},
-    defer: true,
+  const blogPostTemplate = path.resolve(`src/templates/post.js`)
+  return graphql(`
+    query {
+      allSanityPost {
+        edges {
+          node {
+            slug {
+              current
+            }
+          }
+        }
+      }
+    }
+  `, { limit: 1000 }).then(result => {
+    if (result.errors) {
+      throw result.errors
+    }
+    result.data.allSanityPost.edges.forEach(edge => {
+      createPage({
+        path: `${edge.node.slug.current}`,
+        component: blogPostTemplate,
+        context: {
+          slug: edge.node.slug.current
+        },  
+      })
+    })
   })
-}
+};
