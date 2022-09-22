985a5ac6ce03f58ae459b65579964f4cf848f801
William Clark Newell <clark@clarknewell.tech>
2022-09-22 14:34:37 -0600

    Working on index css
    Working on index css

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,6 @@ import Layout from "../components/layout"
 import BlogList from "../components/blog-list"
 import Carousel from "../components/carousel"
 import Seo from "../components/seo"
-import styled from "styled-components"
 import Unicorn from "../assets/unicorn.inline.svg"
 import Diamond from "../assets/diamond.inline.svg"
 import Mission from "../assets/mission.inline.svg"
@@ -39,28 +38,32 @@ const IndexPage = ({ data }) => {
   return (
     <Layout>
       <div className="top-content">
+        <div className="top-grid">
           <Unicorn className="svg"/>
-        <h1>
-          Well hello there, have you <b>met me?</b>
-        </h1>
-        <Diamond className="svg"/>
-        <h2>
-          What if your next hire came with 27 years of <b>customer service</b> experience?
-        </h2>
-        <Mission className="svg"/>
-        <h2>
-          What if that came wrapped with 16 years of <b>leadership,</b> 7 years of <b>project management</b> and 4 years of <b>web development</b> experience?
-        </h2>
-        <Link to="/contact">
-          <button className="repo-link"><b>Find Out!</b></button>
-        </Link>
+          <h1>
+            Well hello there, have you <b>met me?</b>
+          </h1>
+          <Diamond className="svg"/>
+          <h2>
+            What if your next hire came with 28 years of <b>customer service</b> experience?
+          </h2>
+          <Mission className="svg"/>
+          <h2>
+            What if that came wrapped with 16 years of <b>leadership,</b> 8 years of <b>project management</b> and 4 years of <b>web development</b> experience?
+          </h2>
+          <Link to="/about">
+            <button className="repo-link"><b>Learn More</b></button>
+          </Link>
+        </div>
       </div>  
       <div className="mid-content">
-      <h3>Recent Blog Posts</h3>
-        <BlogList posts={posts} />
-        <Link to="/blog" >
-          <h3 className="blog-link" aria-label="see more posts">See more posts...</h3>
-        </Link>
+      <h3 className="section-header">Recent Blog Posts</h3>
+        <div className="blog-posts">
+          <BlogList posts={posts} />
+          <Link to="/blog" >
+            <h3 className="blog-link" aria-label="see more posts">See more posts...</h3>
+          </Link>
+        </div>
       </div>
       <div className="bottom-content">
         <h1 className="carousel-head">What Collaborators are Saying</h1>
@@ -73,8 +76,4 @@ const IndexPage = ({ data }) => {
 export const Head = () => <Seo title="Home" />
 
 export default IndexPage
-
-const CallCard = styled.div`
-  
-`
 
